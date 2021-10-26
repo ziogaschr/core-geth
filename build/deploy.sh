1b45180b8747867a2ab3b7eb17ae3c614cef0845
Chris Ziogas <ziogaschr@gmail.com>
2021-10-27 01:20:51 +0300

    🪃

--- build/deploy.sh
+++ build/deploy.sh
@@ -4,13 +4,13 @@
 GETH_ARCHIVE_NAME="core-geth-${BUILD_OS_NAME}"
 ALLTOOLS_ARCHIVE_NAME="core-geth-alltools-${BUILD_OS_NAME}"
 
-if [[ "${BUILD_OS_NAME}" == "windows" ]]; then
-  7z a "$GETH_ARCHIVE_NAME.zip" build/bin/geth
+if [[ "${BUILD_OS_NAME}" == "win64" ]]; then
+  7z a "$GETH_ARCHIVE_NAME.zip" ./build/bin/geth.exe
 
   sha256sum $GETH_ARCHIVE_NAME.zip
   sha256sum $GETH_ARCHIVE_NAME.zip > $GETH_ARCHIVE_NAME.zip.sha256
 
-  7z a "$ALLTOOLS_ARCHIVE_NAME.zip" build/bin/*
+  7z a "$ALLTOOLS_ARCHIVE_NAME.zip" ./build/bin/*
 
   sha256sum $ALLTOOLS_ARCHIVE_NAME.zip
   sha256sum $ALLTOOLS_ARCHIVE_NAME.zip > $ALLTOOLS_ARCHIVE_NAME.zip.sha256
