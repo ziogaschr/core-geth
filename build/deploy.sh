a670ed5be76d773bff28737f34b4be7aba0758e4
Chris Ziogas <ziogaschr@gmail.com>
2021-10-26 20:56:30 +0300

    🪃

--- build/deploy.sh
+++ build/deploy.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-GETH_ARCHIVE_NAME="core-geth-${TRAVIS_OS_NAME}-$(git describe --always)"
+GETH_ARCHIVE_NAME="core-geth-${BUILD_OS_NAME}"
 zip -j "$GETH_ARCHIVE_NAME.zip" build/bin/geth
 
 shasum -a 256 $GETH_ARCHIVE_NAME.zip
 shasum -a 256 $GETH_ARCHIVE_NAME.zip > $GETH_ARCHIVE_NAME.zip.sha256
 
-ALLTOOLS_ARCHIVE_NAME="core-geth-alltools-${TRAVIS_OS_NAME}-$(git describe --always)"
+ALLTOOLS_ARCHIVE_NAME="core-geth-alltools-${BUILD_OS_NAME}"
 zip -j "$ALLTOOLS_ARCHIVE_NAME.zip" build/bin/*
 
 shasum -a 256 $ALLTOOLS_ARCHIVE_NAME.zip
